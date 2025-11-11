f37b671a2830e0e66e3e803769b22eb373eadce6
Mitchell-Brule <mitchellb@uvic.ca>
2025-11-10 21:34:07 -0800

    Refactor service worker for caching and activation

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,16 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("drink-timer-cache").then(cache => {
-      return cache.addAll(["/", "/index.html", "/style.css", "/script.js"]);
-    })
-  );
+const CACHE_NAME = 'drink-timer-v3';
+const FILES = ['./','./index.html','./style.css','./script.js','./manifest.json','./icon-192.png','./icon-512.png'];
+
+self.addEventListener('install', evt => {
+  evt.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES)));
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
-  );
+self.addEventListener('activate', evt=>{
+  evt.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k===CACHE_NAME?null:caches.delete(k)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', evt=>{
+  evt.respondWith(caches.match(evt.request).then(r=>r || fetch(evt.request)));
 });
