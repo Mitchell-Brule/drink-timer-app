4844d73debacc586c23042e6033b18b69230c736
Mitchell-Brule <mitchellb@uvic.ca>
2025-11-10 20:59:19 -0800

    Add service worker for caching and offline support

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'drink-timer-cache-v1';
+const FILES = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', evt => {
+  evt.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', evt => {
+  evt.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null)))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', evt => {
+  evt.respondWith(caches.match(evt.request).then(resp => resp || fetch(evt.request).catch(()=> caches.match('./'))));
+});
