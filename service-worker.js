4039e824f2be5e9dc5d3df5f84c7d805509655ea
Mitchell-Brule <mitchellb@uvic.ca>
2025-11-10 21:09:05 -0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,15 @@
-const CACHE_NAME = 'drink-timer-cache-v1';
-const FILES = [
-  './',
-  './index.html',
-  './style.css',
-  './script.js',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
-];
-
-self.addEventListener('install', evt => {
-  evt.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', evt => {
-  evt.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null)))
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("drink-timer-cache").then(cache => {
+      return cache.addAll(["/", "/index.html", "/style.css", "/script.js"]);
+    })
   );
-  self.clients.claim();
 });
 
-self.addEventListener('fetch', evt => {
-  evt.respondWith(caches.match(evt.request).then(resp => resp || fetch(evt.request).catch(()=> caches.match('./'))));
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
 });
